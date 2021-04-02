f13f3e81f7ab07f0f59a76f5db518b678d875542
lucassobreiro <lucas.16012002@gmail.com>
2021-04-01 22:54:11 -0300

    jogo_de_dados.js melhorado

--- jogo_de_dadosOLD.js
+++ jogo_de_dadosOLD.js
@@ -15,34 +15,83 @@ var J2nome = "";
 var Jnome = true;
 
 function roll () {
-	document.getElementById("J1_id").innerHTML = J1nome + ": " + J1;
-	document.getElementById("J2_id").innerHTML = J2nome + ": " + J2;
-	document.getElementById("p").innerHTML = "<br>";
+		document.getElementById("J1_id").innerHTML = J1nome + ": " + J1;
+		document.getElementById("J2_id").innerHTML = J2nome + ": " + J2;
+		document.getElementById("p").innerHTML = "<br>";
+		dado=Math.floor(Math.random() * 6) + 1;
+	if(dado==1)
+		document.getElementById("dado_id").src = "imagens/dice-1.png";
+	if(dado==2)
+		document.getElementById("dado_id").src = "imagens/dice-2.png";
+	if(dado==3)
+		document.getElementById("dado_id").src = "imagens/dice-3.png";
+	if(dado==4)
+		document.getElementById("dado_id").src = "imagens/dice-4.png";
+	if(dado==5)
+		document.getElementById("dado_id").src = "imagens/dice-5.png";
+	if(dado==6)
+		document.getElementById("dado_id").src = "imagens/dice-6.png";
 	
-	dado=Math.floor(Math.random() * 6) + 1;
-	document.getElementById("dado_id").src = "imagens/dice-" + dado + ".png";
-	dado2=Math.floor(Math.random() * 6) + 1;
-	document.getElementById("dado2_id").src = "imagens/dice-" + dado2 + ".png";
+		dado2=Math.floor(Math.random() * 6) + 1;
+	if(dado2==1)
+		document.getElementById("dado2_id").src = "imagens/dice-1.png";
+	if(dado2==2)
+		document.getElementById("dado2_id").src = "imagens/dice-2.png";
+	if(dado2==3)
+		document.getElementById("dado2_id").src = "imagens/dice-3.png";
+	if(dado2==4)
+		document.getElementById("dado2_id").src = "imagens/dice-4.png";
+	if(dado2==5)
+		document.getElementById("dado2_id").src = "imagens/dice-5.png";
+	if(dado2==6)
+		document.getElementById("dado2_id").src = "imagens/dice-6.png";
 	
-	dadoS = dado + dado2;
-	document.getElementById("dadoS_id").innerHTML = "Soma: " + dadoS;
-	dadoR ++;
+		dadoS = dado + dado2;
+		document.getElementById("dadoS_id").innerHTML = "Soma: " + dadoS;
+		dadoR ++;
 		
 	if(dadoP==0) {
 		dadoP = dadoS;
 		document.getElementById("dadoP_id").innerHTML = "Ponto: " + dadoP;
 	}
-	if(dadoS==7 && dadoR<=1) win();
-	if(dadoS==11 && dadoR<=1) win();
-	if(dadoS==dadoP && dadoR>=2) win();
-	if(dadoS==2 && dadoR<=1) lose();
-	if(dadoS==3 && dadoR<=1) lose();
-	if(dadoS==12 && dadoR<=1) lose();
-	if(dadoS==7 && dadoR>=2) lose();
-	
+	if(dadoS==7) {
+		if(dadoR<=1) {
+			win();
+		}
+	}
+	if(dadoS==11) {
+		if(dadoR<=1) {
+			win();
+		}
+	}
+	if(dadoS==dadoP) {
+		if(dadoR>=2) {
+			win();
+		}
+	}
+	if(dadoS==2) {
+		if(dadoR<=1) {
+			lose();
+		}
+	}
+	if(dadoS==3) {
+		if(dadoR<=1) {
+			lose();
+		}
+	}
+	if(dadoS==12) {
+		if(dadoR<=1) {
+			lose();
+		}
+	}
+	if(dadoS==7) {
+		if(dadoR>=2) {
+			lose();
+		}
+	}
 	if(jogador==1)
 		document.getElementById("jogador_id").innerHTML = J1nome;
-	else
+	if(jogador==2)
 		document.getElementById("jogador_id").innerHTML = J2nome;
 }
 function win () {
@@ -53,7 +102,9 @@ function win () {
 	dadoP = 0;
 	dadoR = 0;
 	jogador ++;
-	if(jogador > 2) jogador = 1;
+	if(jogador > 2) {
+		jogador = 1;
+	}
 }
 function lose () {
 	document.getElementById("p").innerHTML = "Você perdeu!";
@@ -85,8 +136,10 @@ function lose () {
 function cadastrar () {
 	J1nome = String(document.getElementById("J1nome_id").value);
 	J2nome = String(document.getElementById("J2nome_id").value);
-	if(J1nome=="") Jnome = false;
-	if(J2nome=="") Jnome = false;
+	if(J1nome=="")
+		Jnome = false;
+	if(J2nome=="")
+		Jnome = false;
 	if(Jnome==true) {
 		document.getElementById("jogador_id").innerHTML = J1nome;
 		document.getElementById("J1nome_id").style.visibility = "hidden";
